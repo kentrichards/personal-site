9aeec3b30b03e2efb069c4c12b8d368edd411f84
Kent Richards <kent-richards@outlook.com>
2021-03-08 22:24:47 -0400

    updated icons

--- components/emailLink.tsx
+++ components/emailLink.tsx
@@ -6,18 +6,12 @@ const EmailLink = () => (
     title="Shoot me an email"
   >
     <svg
-      fill="transparent"
       stroke="currentColor"
-      viewBox="0 0 24 24"
+      viewBox="0 0 512 512"
       xmlns="http://www.w3.org/2000/svg"
       aria-labelledby="email"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-      ></path>
+      <path d="M440 6.5L24 246.4c-34.4 19.9-31.1 70.8 5.7 85.9L144 379.6V464c0 46.4 59.2 65.5 86.6 28.6l43.8-59.1 111.9 46.2c5.9 2.4 12.1 3.6 18.3 3.6 8.2 0 16.3-2.1 23.6-6.2 12.8-7.2 21.6-20 23.9-34.5l59.4-387.2c6.1-40.1-36.9-68.8-71.5-48.9zM192 464v-64.6l36.6 15.1L192 464zm212.6-28.7l-153.8-63.5L391 169.5c10.7-15.5-9.5-33.5-23.7-21.2L155.8 332.6 48 288 464 48l-59.4 387.3z" />
     </svg>
   </a>
 )
